98c30ce1b7dc503477c2073744cdaee441ab44b5
f3rnand0 <fgucutito@gmail.com>
2021-04-01 16:46:30 -0500

    First commit

---
+++ src/os/EnvironmentVariables.java
@@ -0,0 +1,21 @@
+package os;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class EnvironmentVariables {
+
+	public static void main(String[] args) throws Exception {
+
+		Map<String, String> envMap = System.getenv();
+		SortedMap<String, String> sortedEnvMap = new TreeMap<String, String>(envMap);
+		Set<String> keySet = sortedEnvMap.keySet();
+		for (String key : keySet) {
+			String value = envMap.get(key);
+			System.out.println("[" + key + "] : " + value);
+		}
+		System.out.println("Java TMP directory: " + System.getProperty("java.io.tmpdir"));
+	}
+}
